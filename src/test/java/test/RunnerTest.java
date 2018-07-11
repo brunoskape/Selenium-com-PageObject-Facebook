dd96e5dfd1bd46662db2fc12544994816788692b
Bruno <Bruno@DESKTOP-TPNKKVV>
2018-07-11 01:20:35 -0300

    Adição de testes

---
+++ src/test/java/test/RunnerTest.java
@@ -0,0 +1,60 @@
+package test;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+
+import br.com.facebook.pages.BasePage;
+import br.com.facebook.pages.FacebookPage;
+import br.com.facebook.pages.LoginPage;
+
+public class RunnerTest {
+	
+    private WebDriver driver;
+	
+	@Before
+	public void setUp() {
+		driver = BasePage.createChrome();
+		new LoginPage(driver).fazerLogin("treinoselenium@outlook.com", "treinamento1");
+		new LoginPage(driver).clicarLogar();
+		
+	}
+
+	
+	@Test
+	public void inserirPubliacaoProprioPerfil() throws InterruptedException {
+		new FacebookPage(driver).acessarPaginaInicial();
+		new FacebookPage(driver).abreCriarPublicacao();
+		new FacebookPage(driver).inserirPublicacaoProprioPerfil();
+	}
+	
+	@Test
+	public void entrarPerfil() throws InterruptedException {
+		new FacebookPage(driver).pesquisar();
+		new FacebookPage(driver).entrarNoPerfilDeOutro();
+		
+	}
+	
+	@Test
+	public void inserirPublicacaoPerfilDeOutro() throws InterruptedException {
+		new FacebookPage(driver).pesquisar();
+		new FacebookPage(driver).entrarNoPerfilDeOutro();
+		new FacebookPage(driver).inserirPublicacaoOutroPerfil();
+		
+	}
+	
+	@Test
+	public void pesquisarNome() throws InterruptedException {
+		new FacebookPage(driver).pesquisar();
+		
+    }
+	
+/*	
+ * implementar o metodo abaixoo
+ * @Test 
+	public void mensagemChat() throws InterruptedException {
+		
+	}
+*/
+}
