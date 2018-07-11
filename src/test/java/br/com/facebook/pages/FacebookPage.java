ce0cc1416e6daaee10e70c9b86c78e3d8a1ac096
Bruno <Bruno@DESKTOP-TPNKKVV>
2018-07-11 00:59:01 -0300

    FacebookPage atualizada.

--- src/test/java/br/com/facebook/pages/FacebookPage.java
+++ src/test/java/br/com/facebook/pages/FacebookPage.java
@@ -65,7 +65,6 @@ public class FacebookPage extends BasePage{
 	
 		assertTrue(nome.contains ("Bruno"));
 	
-
 		
 		return this;
 		
@@ -125,5 +124,5 @@ public class FacebookPage extends BasePage{
 		return this;
 	}
 	
-			
+		
 }
